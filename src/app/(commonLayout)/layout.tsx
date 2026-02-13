4a824535b2905d700cfb5559e45ed70bb23d3fbc
Ridoan-75 <ridoan437@gmail.com>
2026-02-13 10:41:59 +0600

    props err solved

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from "react";
 import { Footer } from "@/src/components/layout/footer";
 import { Header } from "@/src/components/layout/header";
-import { userService } from "@/src/services/user.service";
 
 export const dynamic = "force-dynamic";
 
@@ -9,23 +8,10 @@ interface CommonLayoutProps {
   children: ReactNode;
 }
 
-type UserRole = "ADMIN" | "CUSTOMER" | undefined;
-
-const getUserRole = async (): Promise<UserRole> => {
-  try {
-    const { data } = await userService.getSession();
-    return data?.user?.role as UserRole;
-  } catch {
-    return undefined;
-  }
-};
-
 export default async function CommonLayout({ children }: CommonLayoutProps) {
-  const userRole = await getUserRole();
-
   return (
     <div className="relative min-h-screen flex flex-col bg-background">
-      <Header userRole={userRole} />
+      <Header />
 
       <main className="flex-1">{children}</main>
 
