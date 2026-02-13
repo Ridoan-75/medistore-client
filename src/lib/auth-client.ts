b0fd17d10892c59eeecc9ee380db0a583777c32f
Ridoan-75 <ridoan437@gmail.com>
2026-02-13 21:03:01 +0600

    auth-client changed

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,7 +3,7 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 import { env } from "./../env";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000",
   plugins: [
     inferAdditionalFields({
       user: {
