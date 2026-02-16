f09c318a2f8c518b66bd117486be420271a190d5
Ridoan-75 <ridoan437@gmail.com>
2026-02-16 18:41:40 +0600

    [auth-client]-fix

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,15 @@ const nextConfig = {
       },
     ],
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
